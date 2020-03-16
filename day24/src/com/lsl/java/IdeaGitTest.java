cf5f741d6f2e981c986ef891d03e67853dc4d775
userName <506340601@qq.com>
2020-03-16 15:34:18 +0800

    2020.3.16更新day24下的java包

--- day24/src/com/lsl/java/IdeaGitTest.java
+++ day24/src/com/lsl/java/IdeaGitTest.java
@@ -1,8 +1,13 @@
 package com.lsl.java;
 
 /**
+ * 为什么最迷人的最危险，
  * @author Long
  * @Date 2020/3/15 0:10
  */
 public class IdeaGitTest {
+    public static void main(String[] args) {
+
+        System.out.println("为什么最迷人的最危险!");
+    }
 }
