37dd294e2bf91a8c360193c0559c70569bc0eac9
userName <506340601@qq.com>
2020-03-16 15:54:01 +0800

    3.16日HashMap的操作更新

---
+++ day24/src/com/lsl/java/MapTest.java
@@ -0,0 +1,20 @@
+package com.lsl.java;
+
+/**
+ *
+ *
+ *  面试题：
+ *      1、HashMap的底层实现原理？
+ *  *      2、HashMap和Hashtable的异同
+ *
+ * @author Long
+ * @Date 2020/3/16 15:40
+ */
+public class MapTest {
+    public static void main(String[] args) {
+        System.out.println("你在我眼中真的很特别");
+        System.out.println("为什么最迷人的最危险");
+        System.out.println("我爱你却变成残缺");
+    }
+
+}
